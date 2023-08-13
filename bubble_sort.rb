9b139e6be55bf0a43a988b089fe640ea2d514e96
Abdullah Alattas <86368009+i4pg@users.noreply.github.com>
2023-08-13 11:20:46 +0400

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,15 @@
-def bubble_sort(numbers, result = [])
-  result = numbers.each_with_index do |_num, _index|
-    numbers.each_with_index do |_n, i|
-      next if numbers[i] == numbers.last
-      next unless numbers[i] > numbers[i + 1]    
-      numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+# frozen_string_literal: true
+
+def bubble_sort(numbers)
+  (numbers.length - 1).times do
+    (numbers.length - 1).times do |i|
+      next unless numbers[i] > numbers[i + 1]
+
+      tmp = numbers[i + 1]
+      numbers[i + 1] = numbers[i]
+      numbers[i] = tmp
     end
   end
-  p result
 end
+
 bubble_sort([4, 3, 78, 2, 0, 2])
